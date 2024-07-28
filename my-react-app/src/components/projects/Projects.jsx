74b16f48a76cd492298e0af7a9b44d5ecec3956c
Amanmandahad <mandahadaman>
2024-07-28 20:11:16 +0530

    "new changes"

--- my-react-app/src/components/projects/Projects.jsx
+++ my-react-app/src/components/projects/Projects.jsx
@@ -1,15 +1,55 @@
 import React from 'react'
 import styles from './Projects.module.css'
+import porfolio from '../Images/portfolio.img.png'
 const Projects = () => {
   return (
     <div>
-      projectx
       <div className={styles.container}>
-      </div>
-     
-      <div className={styles.contain}>
+      <div className="container" >
+        <div className="row  justify-content-around" >
+          <div className="col-md-4" style={{margin:"15px"}} >
+            <img src={porfolio} className='img-fluid'  alt="" />
+            <div className={styles.cards}>
+            <button>Code</button>
+            <button>Demo</button>
+          </div>
+          </div>
+          <div className="col-md-4" style={{margin:"15px"}}>
+            <img src={porfolio} className='img-fluid' alt="" />
+            <div className={styles.cards}>
+            <button>Code</button>
+            <button>Demo</button>
+          </div>
+          </div>
+          
+        </div>
+      </div> 
+      <br />
+      <br />
+
+
+      <div className="container">
+        <div className="row  justify-content-around" >
+          <div className="col-md-4" style={{margin:"15px"}}>
+            <img src={porfolio} className='img-fluid' alt="" />
+            <div className={styles.cards}>
+            <button>Code</button>
+            <button>Demo</button>
+          </div>
+          </div>
+          <div className="col-md-4" style={{margin:"15px"}}>
+            <img src={porfolio} className='img-fluid' alt="" />
+            <div className={styles.cards}>
+            <button>Code</button>
+            <button>Demo</button>
+          </div>
+          </div>
+          
+        </div>
+      </div> 
 
       </div>
+     
     </div>
   )
 }
